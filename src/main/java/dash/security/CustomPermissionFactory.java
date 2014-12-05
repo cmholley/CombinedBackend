84881f8b2c4b725b92417ff5267ebf4242b15bf6
CarlSteven <carl_steven@live.com>
2014-12-05 14:34:53 -0600

    Fixed an issue of the browser 401 asking for credentials.
    Needs further testing.

--- src/main/java/dash/security/CustomPermissionFactory.java
+++ src/main/java/dash/security/CustomPermissionFactory.java
@@ -3,8 +3,8 @@ package dash.security;
 import org.springframework.security.acls.domain.DefaultPermissionFactory;
 
 public class CustomPermissionFactory extends DefaultPermissionFactory {
-	 public CustomPermissionFactory() {
-	 super();
-	 registerPublicPermissions(CustomPermission.class);
-	 }
+	public CustomPermissionFactory() {
+		super();
+		registerPublicPermissions(CustomPermission.class);
 	}
+}
